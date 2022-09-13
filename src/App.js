0b84000ac16bf4d1f14422c372dbcd2f3e770e5d
macbook <macbook@MacBook-Pro-macbook.local>
2022-09-13 14:15:57 +0300

    sixth commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: []
+    }
+  }
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+    .then(res => {
+      const users = res.data;
+      this.setState( {users} )
+      // console.log(res.data)
+      // console.log( {res} )
+    })
+  }
+  render() {
+    return (
+      <div>
+        { this.state.users.map( person => 
+          <p key={person.id}>{person.name}
+          <span> {person.email}</span>
+          <span> {person.address.city}</span>
+          <span> {person.address.street}</span>
+          </p>
+        ) }
+      </div>
+    );
+  }
 }
-
 export default App;
+
+
+
+
+
+
+
+// res это сокращенно от result 
+// https://jsonplaceholder.typicode.com/users 
+// обратить внимание, здесь НЕТ ;
+// кладем все в тег <p> <span></span> </p> чтобы отобразить все на одной строке
